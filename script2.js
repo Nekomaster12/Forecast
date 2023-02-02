5d032777087eebb6577dc21599600a61e04ea896
Nekomaster12 <artemmozorow@gmail.com>
2023-02-02 19:21:33 +0300

    lublu obeme

--- script2.js
+++ script2.js
@@ -2,6 +2,7 @@ const HTML_ELEMENTS = {
     CITY_INPUT: document.querySelector('#cityInput'),
     FORECAST_FORM: document.querySelector('.forecast-form'),
     DEGREES_NUM: document.querySelector('.degrees-num'),
+    ADD_TO_FAVORITE_BUTTON: document.querySelector('#heartIcon')
 }
 
 const ERRORS = {
@@ -11,61 +12,136 @@ const ERRORS = {
 
 const fetchData = {
     serverUrl: 'http://api.openweathermap.org/data/2.5/weather',
-    cityName: 'Moscow',
+    cityName: '',
     apiKey: 'd3cdffaa67091758b8f1186dc0cbf511',
 }
 fetchData.apiUrl = `${fetchData.serverUrl}?q=${fetchData.cityName}&appid=${fetchData.apiKey}`
 
-function refreshApiUrl(){
+const cityList = []
+
+function refreshApiUrl() {
     fetchData.apiUrl = `${fetchData.serverUrl}?q=${fetchData.cityName}&appid=${fetchData.apiKey}`
 }
 
-function getCityName(){
-    if(!isCityInputCorrect()){throw ERRORS.CITY_INPUT_ERROR}
-    else{
+function getCityName() {
+    if (!isCityInputCorrect()) {
+        throw ERRORS.CITY_INPUT_ERROR
+    } else {
         fetchData.cityName = HTML_ELEMENTS.CITY_INPUT.value.trim()
     }
 }
 
-function isCityInputCorrect(){
-    let inputValue = HTML_ELEMENTS.CITY_INPUT.value 
+function isCityInputCorrect() {
+    let inputValue = HTML_ELEMENTS.CITY_INPUT.value
     inputValue = inputValue.trim()
-    return(inputValue != '' && isNaN(Number(inputValue)))
+    return (inputValue != '' && isNaN(Number(inputValue)))
 }
 
-async function setWeatherInHtml(){
+function changeCityTemp(weatherData){
+    refreshApiUrl();
+    HTML_ELEMENTS.DEGREES_NUM.textContent = Math.round(weatherData.main.temp - 273)
+}
+
+async function setWeatherInHtml() {
     getCityName();
     refreshApiUrl();
-    changeCityTitles();
     let weatherData = await getWeatherData()
-    if(weatherData != null){
-        HTML_ELEMENTS.DEGREES_NUM.textContent = Math.round(weatherData.main.temp - 273)
-    }else{
+    if (weatherData != null) {
+        changeCityTemp(weatherData);
+        changeCityTitles();
+    } else {
         throw ERRORS.CITY_INPUT_ERROR
     }
 }
 
-function changeCityTitles(){
-    for(item of document.querySelectorAll('.city-name')){
+function changeCityTitles() {
+    for (item of document.querySelectorAll('.city-name')) {
         item.textContent = fetchData.cityName
     }
 }
 
-async function getWeatherData(){
+async function getWeatherData() {
     let weatherData = null;
-    if(!isCityInputCorrect){
+    if (!isCityInputCorrect) {
         throw ERRORS.CITY_INPUT_ERROR
-    }else{
-     await fetch(fetchData.apiUrl)
-     .then(async function(response){
-        if(response.status == 200){
-            weatherData = await response.json()
-            console.log(weatherData)
-        }
-     })
-     .catch(() => {throw ERRORS.RESPONSE_ERROR})
+    } else {
+        await fetch(fetchData.apiUrl)
+            .then(async function (response) {
+                if (response.status == 200) {
+                    weatherData = await response.json()
+                    console.log(weatherData)
+                }
+            })
+            .catch(() => {
+                throw ERRORS.RESPONSE_ERROR
+            })
     }
     return weatherData;
 }
 
+function renderCityList(name) {
+    while (document.querySelector(".locations-items").childNodes.length != 0) {
+        for (let item of document.querySelector(".locations-items").childNodes) {
+            item.remove()
+        }
+    }
+    while (cityList.includes(name)) {
+
+        for (let index = 0; index < cityList.length; index++) {
+            if (cityList[index] === name) {
+                cityList.splice(index, 1)
+            }
+        }
+        for (let item of cityList) {
+            addCityToHTML(item)
+        }
+    }
+};
+
+function checkAddedCitiesCount(){
+    if(cityList.length >= 8){
+        alert("Добавлено слишком много городов. Уберите лишние.")
+        return false;
+    }
+    return true
+}
+
+async function switchToFavoriteCity(item){
+    fetchData.cityName = item.textContent.slice(0, -1)
+    refreshApiUrl();
+    changeCityTitles()
+    let weatherData = await getWeatherData()
+    changeCityTemp(weatherData)
+}
+
+function addCityToHTML(name) {
+    if(checkAddedCitiesCount()){
+    let city = document.createElement("li")
+    city.textContent = name
+    let closer = document.createElement("div")
+    closer.textContent = '✕'
+    closer.className = 'deleteLocation'
+    closer.addEventListener('click', function () {
+        renderCityList(name)
+    })
+    city.append(closer)
+    city.addEventListener('click', function () {
+        switchToFavoriteCity(city)
+    })
+    document.querySelector('.locations-items').append(city)
+    }
+}
+
+async function addToList() {
+    let weatherData = await getWeatherData()
+    if(weatherData != null){
+    if (fetchData.cityName != '' && !cityList.includes(fetchData.cityName)) {
+        cityList.push(fetchData.cityName)
+        addCityToHTML(fetchData.cityName)
+        }
+    }
+}
 HTML_ELEMENTS.FORECAST_FORM.addEventListener('submit', setWeatherInHtml);
+HTML_ELEMENTS.ADD_TO_FAVORITE_BUTTON.addEventListener('click', addToList);
+
+// сделать чтобы по клику на город менялась погода
